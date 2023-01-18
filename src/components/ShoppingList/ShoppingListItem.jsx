578582b34346a5d1ecdc8317a7ecd6b62cec64d2
Tanner Richards <Tanner.richards990@gmail.com>
2023-01-17 17:26:06 -0800

    built out shoppinglistitem component, working on testing it now

--- src/components/ShoppingList/ShoppingListItem.jsx
+++ src/components/ShoppingList/ShoppingListItem.jsx
@@ -1,5 +1,36 @@
-export default function ShoppingListItem() {
-  return <>
-    <div data-testid= "shopping-item"></div>
-  </>;
+import React from 'react';
+
+
+//This function accepts the following props: 
+//onUpdateShoppingItem, onDeleteShoppingItem, shoppingItem. 
+//Shopping item is an object that contains the id, name, etc.
+export default function ShoppingListItem({
+  onUpdateShoppingItem,
+  onDeleteShoppingItem,
+  shoppingItem,
+}) {
+  //This function is called when the user types in the input field.
+  const handleUpdate = (event) => {
+    onUpdateShoppingItem(shoppingItem.id, {
+      ...shoppingItem,
+      name: event.target.value,
+    });
+  };
+  //This function is called when the user clicks the delete button.
+  const handleDelete = () => {
+    onDeleteShoppingItem(shoppingItem.id);
+  };
+  //This function returns the following JSX:
+  return <li>
+    <input
+      type="text"
+      value={shoppingItem.name}
+      onChange={handleUpdate}
+    />
+    <button onClick={handleDelete}>Delete</button>
+  </li>;
 }
+//This function is called when the user clicks the delete button.
+
+
+
