2b4ebcc5211cc31719c7d96a6831ce105bb05793
Tanner Richards <Tanner.richards990@gmail.com>
2023-01-20 15:09:01 -0800

    built out the shoppinglistitemform, working on test next

--- src/components/ShoppingList/ShoppingListItemForm.jsx
+++ src/components/ShoppingList/ShoppingListItemForm.jsx
@@ -1,3 +1,41 @@
+import React, { useState } from 'react';
+import ShoppingListItem from './ShoppingListItem';
+
 export default function ShoppingListItemForm() {
-  return <></>;
+  const [shoppingItems, setShoppingItems] = useState([
+    { id: 1, name: 'item 1' },
+    { id: 2, name: 'item 2' },
+    { id: 3, name: 'item 3' },
+  ]);
+
+  const handleUpdateShoppingItem = (id, updatedShoppingItem) => {
+    setShoppingItems((prevShoppingItems) =>
+      prevShoppingItems.map((shoppingItem) =>
+        shoppingItem.id === id ? updatedShoppingItem : shoppingItem
+      )
+    );
+  };
+
+  const handleDeleteShoppingItem = (id) => {
+    setShoppingItems((prevShoppingItems) =>
+      prevShoppingItems.filter(
+        (shoppingItem) => shoppingItem.id !== id
+      )
+    );
+  };
+
+  return (
+    <div>
+      <ul>
+        {shoppingItems.map((shoppingItem) => (
+          <ShoppingListItem
+            key={shoppingItem.id}
+            shoppingItem={shoppingItem}
+            onUpdateShoppingItem={handleUpdateShoppingItem}
+            onDeleteShoppingItem={handleDeleteShoppingItem}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 }
